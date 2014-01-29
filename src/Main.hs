cba6dda03c75e39b82e6df94eb346ed122222810
Phil Hargett <phil@haphazardhouse.net>
2014-01-29 19:39:52 +0000

    Initial commit

---
+++ src/Main.hs
@@ -0,0 +1,8 @@
+-- | Main entry point to the application.
+module Main where
+
+-- | The main entry point.
+main :: IO ()
+main = do
+    putStrLn "Welcome to FP Haskell Center!"
+    putStrLn "Have a good day!"
