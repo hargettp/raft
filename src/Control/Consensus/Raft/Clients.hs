2997c13bea6038fc0a92f59a922eeb25c23d462f
Phil Hargett <phil@haphazardhouse.net>
2014-08-23 23:43:31 +0000

    Refine signature of methods on Clients structure

--- src/Control/Consensus/Raft/Clients.hs
+++ src/Control/Consensus/Raft/Clients.hs
@@ -48,26 +48,26 @@ type Clients = M.Map Name ClientLog
 mkClients :: Clients
 mkClients = M.empty
 
-isClientRequestAppended :: (Name,Index) -> Clients -> Bool
-isClientRequestAppended (name,index) clients = case (M.lookup name clients)
+isClientRequestAppended :: Name -> Index -> Clients -> Bool
+isClientRequestAppended name index clients = case (M.lookup name clients)
     of Just client -> index <= clientAppendedIndex client
        Nothing -> False
 
-appendClientRequest :: (Name,Index) -> Clients -> Clients
-appendClientRequest (name,index) clients =
+appendClientRequest :: Name -> Index -> Clients -> Clients
+appendClientRequest name index clients =
     case (M.lookup name clients) of
         Just client -> let oldIndex = clientAppendedIndex client
                            in M.insert name (client {clientAppendedIndex = max index oldIndex}) clients
         Nothing -> M.insert name (ClientLog index  (-1 :: Index)) clients
 
-isClientRequestCommitted :: (Name,Index) -> Clients -> Bool
-isClientRequestCommitted (name,index) clients = case (M.lookup name clients)
+isClientRequestCommitted :: Name -> Index -> Clients -> Bool
+isClientRequestCommitted name index clients = case (M.lookup name clients)
     of Just client -> index <= clientCommittedIndex client
        Nothing -> False
 
-commitClientRequest :: (Name,Index) -> Clients -> Clients
-commitClientRequest (name,index) clients = 
-    let newClients = appendClientRequest (name,index) clients
+commitClientRequest :: Name -> Index -> Clients -> Clients
+commitClientRequest name index clients = 
+    let newClients = appendClientRequest name index clients
     in case (M.lookup name newClients) of 
         Just client -> let oldIndex = clientCommittedIndex client
                            in M.insert name (client {clientCommittedIndex = max index oldIndex}) clients
